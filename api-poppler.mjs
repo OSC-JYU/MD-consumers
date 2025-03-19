8e9e3c513f2038c78328703206052421f788bcfb
Ari Häyrinen <ari.hayrinen@gmail.com>
2025-03-19 16:19:02 +0200

    PDF processing now with preloaded data

--- api-poppler.mjs
+++ api-poppler.mjs
@@ -21,6 +21,7 @@ import {
     AckPolicy,
     JSONCodec
   } from "nats";
+import { info } from 'node:console';
 
 // consumer and service name
 const NAME = process.env.NAME 
@@ -167,53 +168,138 @@ async function process_msg(service_url, message) {
         console.log(service_url)
         console.log('**************** POPPLER api ***************')
         console.log(data)
+
+        // upload file to service
+        var formUpload = new FormData();
+        if(data.target) {
+            var readpath = await getFile(MD_URL, data.target, data.userId)
+            const readStream = fs.createReadStream(readpath);
+            formUpload.append('file', readStream);
+        }
         
+        // preload file to service for faster processing 
+        var up_url = `${service_url}/upload`
+
+        const r = await got.post(up_url, {
+            body: formUpload,
+            headers: formUpload.getHeaders(),
+        }).json();
+
+        data.preloaded = r.upload
+        var total_pages = null
+        var info_success = false
+        if(r.info && r.info.pages) {
+            total_pages = parseInt(r.info.pages, 10)
+            info_success = true
+        }
+
+        // then we start to loop through pages
+        // if we have pages defined in params, we loop through them
+        // if not, we loop through all pages and stop when there are no result any more
+
+        const STEP = 1
 
         var firstPageToConvert = 1
-        var lastPageToConvert = 10
+        var lastPageToConvert = 1
+
         if(data.params.firstPageToConvert) 
             firstPageToConvert = parseInt(data.params.firstPageToConvert, 10)
         if(data.params.lastPageToConvert) 
             lastPageToConvert = parseInt(data.params.lastPageToConvert, 10)
 
+        if(firstPageToConvert > lastPageToConvert) {
+            lastPageToConvert = firstPageToConvert
+        }
+
+        const LIMIT = lastPageToConvert - firstPageToConvert 
+        lastPageToConvert = firstPageToConvert
+
         var response_count = 1
-        var count = 0 // safety measure to prevent infinite loop
-        // we make requests in batches of 10 to Poppler until there are no file URIs in the response
-        while(response_count) {
+        var loop_count = 0 // safety measure to prevent infinite loop
+
+        while(firstPageToConvert <= total_pages && loop_count <= LIMIT) {
+
             data.params.firstPageToConvert = firstPageToConvert.toString()
             data.params.lastPageToConvert = lastPageToConvert.toString()
             var formData = new FormData();
-            if(data.target) {
-                var readpath = await getFile(MD_URL, data.target, data.userId)
-                const readStream = fs.createReadStream(readpath);
-                formData.append('content', readStream);
-            }
             formData.append('request', JSON.stringify(data), {contentType: 'application/json', filename: 'request.json'});
-
 
             // send payload to service endpoint 
             var url = `${service_url}/process`
-            console.log(url)
-            const response = await got.post(url, {
+            const file_list = await got.post(url, {
                 body: formData,
                 headers: formData.getHeaders(),
-            });
+            }).json();
             
-    
-            //console.log(response)
-            const file_list = JSON.parse(response.body)
-            console.log(file_list)
             response_count = file_list.response.uri.length
             await getFilesFromStore(file_list.response, service_url, data, url_md)
 
-            firstPageToConvert = lastPageToConvert + 1
-            lastPageToConvert = lastPageToConvert + 10
-            count++
-            if(count > 50) {
+            firstPageToConvert++
+            lastPageToConvert++
+            if(!info_success) {
+                total_pages = lastPageToConvert
+            }
+            loop_count++
+            if(loop_count > 500) {
                 console.log('infinite loop detected')
                 break
             }
         }
+
+        // delete preloaded file from service
+        if(data.preloaded) {
+            var req = {preloaded: data.preloaded, params: {task: 'delete'}}
+            var del_url = `${service_url}/process`
+            var formDelete = new FormData();
+            formDelete.append('request', JSON.stringify(req), {contentType: 'application/json', filename: 'request.json'});
+    
+            const del_response = await got.post(del_url, {
+                body: formDelete,
+                headers: formDelete.getHeaders(),
+            }).json();
+    
+            console.log(del_response)
+        }
+
+
+
+
+        // we make requests in batches of 10 to Poppler until there are no file URIs in the response
+        // while(response_count) {
+        //     data.params.firstPageToConvert = firstPageToConvert.toString()
+        //     data.params.lastPageToConvert = lastPageToConvert.toString()
+        //     var formData = new FormData();
+        //     // if(data.target) {
+        //     //     var readpath = await getFile(MD_URL, data.target, data.userId)
+        //     //     const readStream = fs.createReadStream(readpath);
+        //     //     formData.append('content', readStream);
+        //     // }
+        //     formData.append('request', JSON.stringify(data), {contentType: 'application/json', filename: 'request.json'});
+
+
+        //     // send payload to service endpoint 
+        //     var url = `${service_url}/process`
+        //     console.log(url)
+        //     const response = await got.post(url, {
+        //         body: formData,
+        //         headers: formData.getHeaders(),
+        //     });
+            
+    
+        //     //console.log(response)
+        //     const file_list = JSON.parse(response.body)
+        //     console.log(file_list)
+        //     response_count = file_list.response.uri.length
+        //     await getFilesFromStore(file_list.response, service_url, data, url_md)
+
+        //     firstPageToConvert = lastPageToConvert + 1
+        //     lastPageToConvert = lastPageToConvert + STEP
+        //     count++
+        //     if(count > 50) {
+        //         console.log('infinite loop detected')
+        //         break
+        //     }
+        // }
 
         
 
